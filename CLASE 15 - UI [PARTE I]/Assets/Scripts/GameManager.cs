8092965deb929d1129dc76027e9e0ac96c6a7d06
Gaston Caminiti <gastoncaminiti@gmail.com>
2021-11-17 14:05:05 -0300

    Commit 0 [ESTRUCTURA INICIAL DEL PROYECTO]
    Proyecto Unity
    .gitignore
    .readme

---
+++ CLASE 15 - UI [PARTE I]/Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    // Start is called before the first frame update
+
+    //SCORE
+    public static int score;
+
+    private int scoreInstanciado; 
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            score = 0;
+            scoreInstanciado = 0;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void addScore()
+    {
+        instance.scoreInstanciado += 1;  
+    }
+
+    public static int GetScore()
+    {
+        return instance.scoreInstanciado;
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# REPOSITORIO CLASE 15 - DESARROLLO DE VIDEOJUEGOS
+
+Proyecto desarrollado en el curso de desarrollo de videojuego de CoderHouse.
+
+Tema: Canvas y elementos de UI
+
+Engine: Unity
